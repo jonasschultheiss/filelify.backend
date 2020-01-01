1a0e9cc588b58e19ad406b74d2d56dd024459834
JonasSchultheiss-EH <jonas.schultheiss@endress.com>
2020-01-01 21:34:12 +0100

    :art: app can now be started without logz token

--- src/commons/logging/logger.js
+++ src/commons/logging/logger.js
@@ -2,7 +2,7 @@ const { createLogger, format, transports } = require('winston');
 const LogzioWinstonTransport = require('winston-logzio');
 
 const logLevelConstants = require('./logLevelConstants');
-const config = require('../../config');
+const config = require('../config');
 
 const {
   combine,
@@ -14,37 +14,53 @@ const {
   simple
 } = format;
 
-const transportConsole = new transports.Console();
-const transportFileErr = new transports.File({
-  filename: `../../../logs/${logLevelConstants.ERROR}.log`,
-  level: logLevelConstants.ERROR
-});
-const transportFileWarn = new transports.File({
-  filename: `../../../logs/${logLevelConstants.WARN}.log`,
-  level: logLevelConstants.WARN
-});
-const transportFileInfo = new transports.File({
-  filename: `../../../logs/${logLevelConstants.INFO}.log`,
-  level: logLevelConstants.INFO
-});
-const transportWebErr = new LogzioWinstonTransport({
-  level: logLevelConstants.ERROR,
-  name: 'winston_logzio',
-  token: config.LOGZIO_TOKEN,
-  host: 'listener.logz.io'
-});
-const transportWebWarn = new LogzioWinstonTransport({
-  level: logLevelConstants.WARN,
-  name: 'winston_logzio',
-  token: config.LOGZIO_TOKEN,
-  host: 'listener.logz.io'
-});
-const transportWebInfo = new LogzioWinstonTransport({
-  level: logLevelConstants.INFO,
-  name: 'winston_logzio',
-  token: config.LOGZIO_TOKEN,
-  host: 'listener.logz.io'
-});
+const winstonTransports = [];
+winstonTransports.push(new transports.Console());
+winstonTransports.push(
+  new transports.File({
+    filename: `../../../logs/${logLevelConstants.ERROR}.log`,
+    level: logLevelConstants.ERROR
+  })
+);
+winstonTransports.push(
+  new transports.File({
+    filename: `../../../logs/${logLevelConstants.WARN}.log`,
+    level: logLevelConstants.WARN
+  })
+);
+winstonTransports.push(
+  new transports.File({
+    filename: `../../../logs/${logLevelConstants.INFO}.log`,
+    level: logLevelConstants.INFO
+  })
+);
+
+if (config.LOGZIO_TOKEN) {
+  winstonTransports.push(
+    new LogzioWinstonTransport({
+      level: logLevelConstants.ERROR,
+      name: 'winston_logzio',
+      token: config.LOGZIO_TOKEN,
+      host: 'listener.logz.io'
+    })
+  );
+  winstonTransports.push(
+    new LogzioWinstonTransport({
+      level: logLevelConstants.WARN,
+      name: 'winston_logzio',
+      token: config.LOGZIO_TOKEN,
+      host: 'listener.logz.io'
+    })
+  );
+  winstonTransports.push(
+    new LogzioWinstonTransport({
+      level: logLevelConstants.INFO,
+      name: 'winston_logzio',
+      token: config.LOGZIO_TOKEN,
+      host: 'listener.logz.io'
+    })
+  );
+}
 
 const logger = createLogger({
   format: combine(
@@ -55,20 +71,24 @@ const logger = createLogger({
     align(),
     simple()
   ),
-  transports: [
-    transportConsole,
-    transportFileInfo,
-    transportFileWarn,
-    transportFileErr,
-    transportWebInfo,
-    transportWebWarn,
-    transportWebErr
-  ],
+  transports: winstonTransports,
   exitOnError: false
 });
 
 logger.INFO = logLevelConstants.INFO;
 logger.WARN = logLevelConstants.WARN;
 logger.ERROR = logLevelConstants.ERROR;
+
+if (config.LOGZIO_TOKEN) {
+  logger.log({
+    level: logLevelConstants.INFO,
+    message: 'logz.io token provided.'
+  });
+} else {
+  logger.log({
+    level: logLevelConstants.ERROR,
+    message: 'logz.io token not provided, will not log to logz.io'
+  });
+}
 
 module.exports = logger;
