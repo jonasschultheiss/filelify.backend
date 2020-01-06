d6535dfeae3bf44b2c0d5e7fdf82923b11e0c2d4
JonasSchultheiss-EH <jonas.schultheiss@endress.com>
2020-01-06 04:10:50 +0100

    :sparkles: token will expire server side

--- src/middleware/jwtVerify.js
+++ src/middleware/jwtVerify.js
@@ -2,11 +2,18 @@ const jwt = require('jsonwebtoken');
 
 const config = require('../commons/config');
 
+const didTokenExpire = token => {
+  const aliveSince = Date.now() - token.iat * 1000;
+  const maxAge = 25 * 60000;
+  return aliveSince > maxAge;
+};
+
 const jwtVerify = async (req, res, next) => {
   if (!req.body.token) res.status(401).send('Token not provided.');
   const { token } = req.body;
   try {
-    await jwt.verify(token, config.JWT_SECRET);
+    const decodedToken = await jwt.verify(token, config.JWT_SECRET);
+    if (didTokenExpire(decodedToken)) res.status(401).send('Token expired.');
   } catch (err) {
     res.status(401).send('Token not valid.');
   }
